3cd62317fc21b410fdf63f3677da237ddfc9b998
InderdeepSync <buntycool1998@gmail.com>
2023-07-15 16:45:21 -0400

    Add check card feature

--- cards/views.py
+++ cards/views.py
@@ -3,8 +3,12 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     ListView, CreateView, UpdateView,
 )
+from django.shortcuts import get_object_or_404, redirect
+
+import random
 
 from .models import Card
+from .forms import CardCheckForm
 
 
 class CardListView(ListView):
@@ -33,4 +37,14 @@ class BoxView(CardListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = CardCheckForm(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data["card_id"])
+            card.move(form.cleaned_data["solved"])
+
+        return redirect(request.META.get("HTTP_REFERER"))
